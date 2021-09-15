4bdd456b6053885e3df7c47255ec7c536b9ac65d
Akshay <akshay.pawar@ambab,com>
2021-09-15 16:34:07 +0530

    form validation donee in jquery

--- form validation/form.js
+++ form validation/form.js
@@ -1,12 +1,41 @@
 $(document).ready(function(){
     $("#submit").click(function(event){
-        if($("#name").val()=="" && $("#email").val()==""){
-            $("#Error").text("inputs fields are mandatory....").show().fadeOut(2000);
-            event.preventDefault();
-        }else{
-            $("#Success").text("Form Submited...").show().fadeOut(2000);
-            event.preventDefault();
+
+        $(".error").remove();
+        
+        if($("#name").val()==""){
+            $("#nameErr").html("Name is required");
         }
+
+        if($("#email").val()==""){
+            $("#emailErr").html("Email is required");
+        }
+         if($("#mob").val()==""){
+            $("#mobileErr").html("Mobile no. is required");
+        }
+         if($("#pass1").val()==""){
+            $("#passErr").html("Password is required");
+        }
+         if($("#pass2").val()==""){
+
+            $("#passErr2").html("Password is required");
+        }else{
+
+             if($("#pass1").val().length!==$("#pass2").val().length)
+            {
+                $("#passErr2").text("Both Password range should be same");
+            }
+        }
+        
+        if ((!($('#r1').prop('checked'))) && (!($('#r2').prop('checked'))) && (!($('#r3').prop('checked')))) {
+            $("#radioErr").text("Select gender is required");
+        } 
+
+        if($("#message").val()==""){
+            $("#msgErr").html("Message is required");
+        }
+
+        event.preventDefault();
     });
 });
 
